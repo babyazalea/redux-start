ceefbf77bac701300ec9c060101443ed6a44b585
babyazalea <usun12@hotmail.com>
2021-01-10 00:16:06 +0900

    switch-case in reducer

--- src/store/reducer.js
+++ src/store/reducer.js
@@ -1,27 +1,26 @@
 const initialState = {
   counter: 0,
+  results: [],
 };
 
 const reducer = (state = initialState, action) => {
-  if (action.type === "INCREMENT") {
-    return {
-      counter: state.counter + 1,
-    };
-  }
-  if (action.type === "DECREMENT") {
-    return {
-      counter: state.counter - 1,
-    };
-  }
-  if (action.type === "ADD") {
-    return {
-      counter: state.counter + action.value,
-    };
-  }
-  if (action.type === "SUBTRACT") {
-    return {
-      counter: state.counter - action.value,
-    };
+  switch (action.type) {
+    case "INCREMENT":
+      return {
+        counter: state.counter + 1,
+      };
+    case "DECREMENT":
+      return {
+        counter: state.counter - 1,
+      };
+    case "ADD":
+      return {
+        counter: state.counter + action.val,
+      };
+    case "SUBTRACT":
+      return {
+        counter: state.counter - action.val,
+      };
   }
   return state;
 };
